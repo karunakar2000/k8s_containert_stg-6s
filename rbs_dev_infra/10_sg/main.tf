1241162ac7990da234f1f6371aa604613f148d53
karunakar2000 <karunakar2000@gmail.com>
2025-11-06 22:10:44 +0200

    code

--- rbs_dev_infra/10_sg/main.tf
+++ rbs_dev_infra/10_sg/main.tf
@@ -1,8 +1,14 @@
-module "catalogue_sg" {
-  source = "terraform-aws-modules/security-group/aws"
-  name        = "${local.resource_name_prefix}-catalogue-sg"
-  use_name_prefix = false
-  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
-  vpc_id      = data.aws_ssm_parameter.vpc_id.value
+# Using open source module:
 
+# module "catalogue_sg" {
+#   source = "terraform-aws-modules/security-group/aws"
+#   name        = "${local.resource_name_prefix}-catalogue-sg"
+#   use_name_prefix = false
+#   description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+#   vpc_id      = data.aws_ssm_parameter.vpc_id.value
+
+# }
+
+module "sg" {
+  source = "git::https://github.com/karunakar2000/k8s_containert_stg-6s.git//terraform_custom_module_sg"
 }

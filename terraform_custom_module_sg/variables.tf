38eaa9fdc98d627446766cfa72511ed5ddb5f733
karunakar2000 <karunakar2000@gmail.com>
2025-11-07 13:38:13 +0200

    code_push

--- terraform_custom_module_sg/variables.tf
+++ terraform_custom_module_sg/variables.tf
@@ -1,52 +1,26 @@
-# variable "project_name" {
-#     type        = string
-# }
-
-# variable "environment" {
-#     type        = string
-# }
-
-# variable "sg_name" {
-#     type        = string
-# }
-
-# variable "sg_description" {
-#     type        = string
-#     default     = ""
-# }
-
-# variable "vpc_id" {
-#     type        = string
-# }
-
-# variable "sg_tags" {
-#     type  = map
-#     default = {}
-# }
-
-
 variable "project_name" {
-  type = string
+    type        = string
 }
 
 variable "environment" {
-  type = string
+    type        = string
 }
 
 variable "sg_name" {
-  type        = string
+    type        = string
 }
 
 variable "sg_description" {
-  type        = string
-  default = ""
+    type        = string
+    default     = ""
 }
 
 variable "vpc_id" {
-    type = string
+    type        = string
 }
 
 variable "sg_tags" {
-    type = map
+    type  = map
     default = {}
 }
+

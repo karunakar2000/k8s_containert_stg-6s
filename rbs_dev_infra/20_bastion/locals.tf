2dc39ade3a02dead58bbd7dbf8ee00c6f645852f
karunakar2000 <karunakar2000@gmail.com>
2025-11-08 21:00:38 +0200

    module_code

--- rbs_dev_infra/20_bastion/locals.tf
+++ rbs_dev_infra/20_bastion/locals.tf
@@ -1,7 +1,7 @@
 locals {
     ami_id = data.aws_ami.devsecops.id
     bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
-    public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+    public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
 
     common_tags = {
         Project = var.project_name

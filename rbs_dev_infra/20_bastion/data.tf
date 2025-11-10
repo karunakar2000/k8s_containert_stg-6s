79ef357e00a7dade8d70649d7cb9740dc5f4e705
karunakar2000 <karunakar2000@gmail.com>
2025-11-10 18:40:37 +0200

    updated_script

--- rbs_dev_infra/20_bastion/data.tf
+++ rbs_dev_infra/20_bastion/data.tf
@@ -17,7 +17,7 @@ data "aws_ami" "devsecops" {
 }
 
 data "aws_ssm_parameter" "bastion_sg_id" {
-  name = "/${var.project_name}-${var.environment}/bastion_sg_id"
+  name = "/${var.project_name}-${var.environment}/bastion-sg_sg_id"
 }
 
 data "aws_ssm_parameter" "public_subnet_ids" {

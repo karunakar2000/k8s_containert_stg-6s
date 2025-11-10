9483bd28a775fcdababac5ae6d9729bbc1b15986
karunakar2000 <karunakar2000@gmail.com>
2025-11-10 21:44:06 +0200

    updated_script

--- rbs_dev_infra/10_sg/main.tf
+++ rbs_dev_infra/10_sg/main.tf
@@ -24,7 +24,8 @@ module "sg" {
 resource "aws_security_group_rule" "frontend_frontend_alb" {
   security_group_id = module.sg[9].sg_id # frontend SG id
   source_security_group_id = module.sg[11].sg_id # frontend LB SG id
+  type              = "ingress"
   from_port         = 80
-  ip_protocol       = "tcp"
+  protocol          = "tcp"
   to_port           = 80
 }

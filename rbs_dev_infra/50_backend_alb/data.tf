3dbfe455a7628c195effaffa905577e3d0a5e815
karunakar2000 <karunakar2000@gmail.com>
2025-11-11 14:51:42 +0200

    pvt_subnets

--- rbs_dev_infra/50_backend_alb/data.tf
+++ rbs_dev_infra/50_backend_alb/data.tf
@@ -6,6 +6,6 @@ data "aws_ssm_parameter" "backend-alb-sg_sg_id" {
   name = "/${var.project_name}-${var.environment}/backend-alb-sg_sg_id"
 }
 
-data "aws_ssm_parameter" "public_subnet_ids" {
-  name = "/${var.project_name}-${var.environment}/public_subnet_ids"
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project_name}-${var.environment}/private_subnet_ids"
 }
--- rbs_dev_infra/50_backend_alb/locals.tf
+++ rbs_dev_infra/50_backend_alb/locals.tf
@@ -2,7 +2,8 @@ locals {
     resource_name_prefix = "${var.project_name}-${var.environment}" # project_name-environment
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     backend-alb-sg_sg_id = data.aws_ssm_parameter.backend-alb-sg_sg_id.value
-    public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+    private_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+
     common_tags = {
         Project = var.project_name
         Environment = var.environment

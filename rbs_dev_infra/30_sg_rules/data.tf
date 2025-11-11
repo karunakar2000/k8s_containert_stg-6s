00d153f61ad9e75279275a8d84ac6e353f692ebb
karunakar2000 <karunakar2000@gmail.com>
2025-11-11 12:15:22 +0200

    sg_rules

--- rbs_dev_infra/30_sg_rules/data.tf
+++ rbs_dev_infra/30_sg_rules/data.tf
@@ -1,3 +1,3 @@
-data "aws_ssm_parameter" "backend-alb-sg" {
-  name = "/${var.project_name}-${var.environment}/backend-alb-sg"
+data "aws_ssm_parameter" "backend-alb-sg_sg_id" {
+  name = "/${var.project_name}-${var.environment}/backend-alb-sg_sg_id"
 }

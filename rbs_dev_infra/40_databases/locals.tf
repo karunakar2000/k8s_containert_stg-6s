24ac08e567a55d1bc35145770bc66496a734f60a
karunakar2000 <karunakar2000@gmail.com>
2025-11-12 17:51:35 +0200

    database

--- rbs_dev_infra/40_databases/locals.tf
+++ rbs_dev_infra/40_databases/locals.tf
@@ -1,4 +1,5 @@
  locals {
+    resource_name_prefix = "${var.project_name}-${var.environment}" # project_name-environment
     ami_id = data.aws_ami.devsecops.id
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
--- rbs_dev_infra/40_databases/main.tf
+++ rbs_dev_infra/40_databases/main.tf
@@ -7,7 +7,9 @@ resource "aws_instance" "mongodb" {
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.project_name}-${var.environment}-mongodb"
+      Name = "${local.resource_name_prefix}-mongodb" # RBS-DEV-mongodb
     }
   )
+
+  
 }

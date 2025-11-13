c975dcd0aa42f9b39c62e6d0338403fd2f7d17ac
karunakar2000 <karunakar2000@gmail.com>
2025-11-13 16:52:41 +0200

    sh_updated

--- rbs_dev_infra/40_databases/main.tf
+++ rbs_dev_infra/40_databases/main.tf
@@ -24,9 +24,17 @@ resource "terraform_data" "mongodb" {
     password  = "DevOps321"
     host      = aws_instance.mongodb.private_ip
   }
+  
+  # Terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
   provisioner "remote-exec" {
     inline = [
-      "echo Hello World"
+      "chmod +x /tmp/bootstrap.sh"
+      "sudo bash /tmp/bootstrap.sh "
     ]
   }
 }

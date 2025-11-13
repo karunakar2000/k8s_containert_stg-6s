660907da02e2505ea743a174b65f786aa2306fe2
karunakar2000 <karunakar2000@gmail.com>
2025-11-13 18:24:46 +0200

    tf2_updated

--- rbs_dev_infra/40_databases/bootstrap.sh
+++ rbs_dev_infra/40_databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dnf install ansible -y
-ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=mongodb main.yml
+ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
